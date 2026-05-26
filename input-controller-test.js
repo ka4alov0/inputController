dc41b56858d9ed0ccf043df6bead49acb1238b85
TkHex <hcelovek2842@gmail.com>
2026-05-26 13:11:35 +0300

    controller

---
+++ input-controller-test.js
@@ -0,0 +1,63 @@
+const attachBtn = document.getElementById('attach')
+const detachBtn = document.getElementById('detach')
+const controllerOn = document.getElementById('controllerOn')
+const controllerOff = document.getElementById('controllerOff')
+const enableActionLeft = document.getElementById('enableActionLeft')
+const disableActionLeft = document.getElementById('disableActionLeft')
+const bindSpace = document.getElementById('bindSpace')
+
+attachBtn.addEventListener('click', (event) => {controller.attach(document)})
+detachBtn.addEventListener('click', (event) => {controller.detach(document)})
+controllerOn.addEventListener('click', (event) => {controller.controllerOn()})
+controllerOff.addEventListener('click', (event) => {controller.controllerOff()})
+enableActionLeft.addEventListener('click', (event) => {controller.enableAction('left')})
+disableActionLeft.addEventListener('click', (event) => {controller.disableAction('left')})
+bindSpace.addEventListener('click', (event) => {controller.bindActions({"jump": {keys: [32]}})})
+
+const controller = new InputController({
+    "left": {
+        keys: [37,65],
+    },
+    "right": {
+        keys: [39,68]
+    }
+}, document)
+
+const gameArea = document.getElementById('gameArea')
+const ctx = gameArea.getContext('2d')
+let block = {
+    x: gameArea.width/2 - 50,
+    y: gameArea.height/2 - 50,
+    width: 100,
+    height: 100,
+    speed: 10,
+    color: 'black'
+}
+
+function drawBlock() {
+    ctx.fillStyle = block.color
+    ctx.fillRect(block.x, block.y, block.width, block.height)
+}
+
+function game() {
+    ctx.clearRect(0, 0, gameArea.width, gameArea.height)
+    if (controller.isActionActive('left')){
+        if (block.x <= 0) {
+            block.x = 0
+        }
+        else block.x -= block.speed
+    }
+    if (controller.isActionActive('right')){
+        if (block.x + block.width >= gameArea.width) {
+            block.x = gameArea.width - block.width
+        }
+        else block.x += block.speed
+    }
+    if (controller.isActionActive('jump')){
+        block.color = 'red'
+    } else block.color = 'black'
+    drawBlock()
+    requestAnimationFrame(game)
+}
+
+game()
--- input-controller.js
+++ input-controller.js
@@ -6,38 +6,54 @@ class InputController{
     static ACTION_ACTIVATED = "input-controller:action-activated"
     static ACTION_DEACTIVATED = "input-controller:action-deactivated"
 
-    constructor(actionsToBind = {}, target=''){
+    constructor(actionsToBind = {}, target=document){
+        this.keyDown = this.keyDown.bind(this)
+        this.keyUp = this.keyUp.bind(this)
         this.target = target
         this.activeActions = new Set()
         this.pressedKeys = new Set()
         this.actions = new Map()
         this.bindActions(actionsToBind)
+        this.attach(target)
     }
 
     bindActions(actionsToBind){
-        for(let key in actionsToBind){
-            this.actions.set(actionsToBind[key])
+        for(let actionName in actionsToBind){
+            this.actions.set(actionName, {keys: actionsToBind[actionName].keys, enabled: true})
         }
+        // console.log(this.actions)
     }
 
     enableAction(actionName){
-
+        // enabled у action делает true, но сначала надо этот enabled добавить
+        this.actions.get(actionName).enabled = true
+        console.log(this.actions.get(actionName).enabled)
     }
 
     disableAction(actionName){
-        
+        // enabled у action делает false
+        this.actions.get(actionName).enabled = false
+        console.log(this.actions.get(actionName).enabled)
     }
 
     attach(target, dontEnable = false){
-
+        this.focused = true
+        if(this.focused){
+            this.target.addEventListener('keydown', this.keyDown)
+            this.target.addEventListener('keyup', this.keyUp)
+        }
     }
 
     detach(){
-
+        this.focused = false
+        this.target.removeEventListener('keydown', this.keyDown)
+        this.target.removeEventListener('keyup', this.keyUp)
+        this.activeActions.clear()
+        this.pressedKeys.clear()
     }
 
     isActionActive(action){
-        if(this.activeActions.has(action)) {
+        if(this.activeActions.has(action)){
             return true
         }
         else return false
@@ -51,44 +67,42 @@ class InputController{
     }
 
     keyDown(event){
-
+        if(this.enabled){
+            for(const [actionName, actionData] of this.actions){
+                if(actionData.enabled === true){
+                    if(actionData.keys.includes(event.keyCode)){
+                        this.pressedKeys.add(event.keyCode)
+                        this.activeActions.add(actionName)
+                    }
+                }
+            }
+        }
+        console.log(this.activeActions)
+        console.log(this.pressedKeys)
     }
 
     keyUp(event){
-
+        if(this.enabled){
+            for(const [actionName, actionData] of this.actions){
+                if(actionData.enabled === true){
+                    if(actionData.keys.includes(event.keyCode)){
+                        this.pressedKeys.delete(event.keyCode)
+                        this.activeActions.delete(actionName)
+                    }
+                }
+            }
+        }
+        console.log(this.activeActions)
+        console.log(this.pressedKeys)
     }
-    
+
+    controllerOn(){
+        this.enabled = true
+    }
+
+    controllerOff(){
+        this.enabled = false
+        this.activeActions.clear()
+        this.pressedKeys.clear()
+    }
 }
-
-const controller = new InputController({
-    "left": {
-        keys: [37,65],
-    },
-    "right": {
-        keys: [39,68]
-    }
-}, '')
-
-console.log(controller.actions)
-
-const gameArea = document.getElementById('gameArea')
-
-const ctx = gameArea.getContext("2d")
-
-document.addEventListener('keydown', controller.keyDown(event))
-document.addEventListener('keyup', controller.keyUp(event))
-
-function game(){
-
-    if (controller.isActionActive('left')) {
-        console.log('left')
-    }
-
-    if (controller.isActionActive('right')) {
-        console.log('right')
-    }
-
-    requestAnimationFrame(game)
-}
-
-game()
